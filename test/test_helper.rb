4d7da064a4ba0097e8136d5998b2f3a9d8e94123
ryym <ryym.64@gmail.com>
2016-07-27 17:10:10 +0900

    Generate test coverage

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,6 +2,16 @@ ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 
+if ENV["COVERAGE"]
+  require "simplecov"
+  SimpleCov.start "rails" do
+    add_filter "/test/"
+    add_filter "/vendor/"
+  end
+  require "codeclimate-test-reporter"
+  CodeClimate::TestReporter.start
+end
+
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
