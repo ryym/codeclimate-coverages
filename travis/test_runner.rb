be59dcc01363256576967f4619bf0d17d1f47b20
ryym <ryym.64@gmail.com>
2016-07-27 17:44:17 +0900

    Send coverage of JS

--- travis/test_runner.rb
+++ travis/test_runner.rb
@@ -6,8 +6,9 @@ require "English"
 # TestRunner
 class TestRunner
   def execute
-    system "COVERAGE=1 bin/rails test"
-    # system "npm run cover"
+    # system "COVERAGE=1 bin/rails test"
+    system "npm run cover"
+    system "$(npm bin)/codeclimate-test-reporter < coverage/lcov.info"
   end
 end
 
